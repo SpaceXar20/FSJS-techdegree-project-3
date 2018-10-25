69168f2e2ab067e11e693848fe0ea57ab4588e5b
Erik <eriklopezramirez@Eriks-MacBook-Pro.local>
2018-10-24 20:00:19 -0400

    Added T-shirt and Workshops sections

--- js/app.js
+++ js/app.js
@@ -24,9 +24,8 @@ let $shirtColor = $("#colors-js-puns");
 //matching JS Puns theme with JS Puns shirts
 $shirtColor.hide();
 $shirtDesign.change(function(){
-if($('#design option:selected').text()==="Theme - JS Puns"){
+if($("#design option:selected").text()==="Theme - JS Puns"){
  $shirtColor.show();
- $("#color").val("cornflowerblue")
  $("#color option[value = cornflowerblue]").show();
  $("#color option[value = darkslategrey]").show();
  $("#color option[value = gold]").show();
@@ -39,11 +38,10 @@ if($('#design option:selected').text()==="Theme - JS Puns"){
 });
 
 //Matching I♥ JS theme with I♥ JS shirts
-
+$shirtColor.hide();
 $shirtDesign.change(function() {
-    if ($("design option:selected").val() === "heart js") {
+    if ($("design option:selected").text() === "Theme - I &#9829; JS") {
       $shirtColor.show();
-      $("#color").val("tomato");
       $("#color option[value = tomato]").show();
       $("#color option[value = steelblue]").show();
       $("#color option[value = dimgrey]").show();
@@ -51,5 +49,111 @@ $shirtDesign.change(function() {
       $("#color option[value = darkslategrey]").hide();
       $("#color option[value = gold]").hide();
   
-    }
+    } else if ($("#design option:selected").text()==="Select Theme") {
+      $shirtColor.hide();   
+}
 });
+
+//Registering for Tech Workshops
+
+//this variable represents the cost of each event
+let totalCost = 0;
+$('.activities').append('<p></p>'); //I append paragraphs  to the activities fieldset so prices can add and shown
+
+//I add $100 to (totalCost) when js frameworks is selected
+$("input[name='js-frameworks']").on("click", function(){
+  if($(this).is(":checked")){
+   totalCost = totalCost + 100; //$100 is added to variable
+   $(".activities p").text("Total: $ " + totalCost);
+   //Prevent conflict between JS Frameworks and Express Workshop
+$("[name='express']").attr("disabled", "true").parent().css({"color":"#ce2121"});
+} else {
+  //take away $100 from totalCost
+  totalCost = totalCost - 100;
+  $(".activities p").text("Total: $ " + cost);
+  $("[name='express']").removeAttr("disabled").parent().css({"color":"#54e565"});
+ }
+});
+
+//The cost will be $100  when the user clicks on express
+$("input[name='express']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+    //When the user chooses express workshop, the javascript frameworks workshop will be unavailable to avoid conflict
+   $("[name='js-frameworks']").attr("disabled", "true").parent().css({"text-decoration":"line-through"});
+  } else {
+    //take away $100 
+    totalCost = totalCost - 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+
+    //if the user doesn't select express then other events will be available
+    $("[name='js-frameworks']").removeAttr("disabled").parent().css({"color": "#54e565"});
+  }
+});
+
+//Add cost to Javascript Libraries
+$("input[name='js-libs']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+    //make a conflicting event unavailable
+    $("[name='node']").attr("disabled", "true").parent().css({"text-decoration": "line-through"});
+  } else {
+    totalCost = totalCost - 100;
+    $("activities p").text("Total Cost: $ " + totalCost);
+    //if the user doesn't select JS libraries then other events will be available
+    $("[name='node']").removeAttr("disabled").parent().css({"color": "#54e565"});
+  }
+});
+
+//Add cost to NODE event
+$("input[name='node']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 100;
+  $(".activities p").text("Total Cost: " + totalCost);
+  //if the user chooses node, then the js libraries event won't be available
+  $("[name='js-libs']").attr("disabled", "true").parent().css({"text-decoration": "line-through"});
+  } else {
+    totalCost = totalCost - 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+    //if the user doesn't choose node, then other events will be available
+    $("[name='js-libs']").removeAttr("disabled").parent().css({"color": "#54e565"});
+  }
+});
+
+//adding total cost for events that don't conflict when a user chooses an event
+//If the user chooses Builds tools workshop
+$("input[name='build-tools']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+  } else {
+    totalCost = totalCost - 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+  }
+});
+
+//When the user chooses the npm workshop
+$("input[name='npm']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 100;
+  } else {
+    totalCost = totalCost - 100;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+  }
+});
+
+//Adding cost when the user chooses the Main Conference
+$("input[name='all']").on("click", function(){
+  if($(this).is(":checked")){
+    totalCost = totalCost + 200;
+  } else {
+    totalCost = totalCost - 200;
+    $(".activities p").text("Total Cost: $ " + totalCost);
+  }
+});
+
+
+
+
